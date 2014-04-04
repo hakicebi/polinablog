e90746b2f2e32a0eec76bc136743bbcc2ed05c7a
Haki Cebi <hakicebi@hotmail.com>
2014-04-04 11:12:08 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PolinaBlog::Application.config.secret_key_base = '3f6304e86c234a8f78bfc2f61f162e968907206f3e2a61c7f224a1aad1d2d0b3e9a57cfe4e19c3cef4cccb604e0b42cca4506e72ab5669453b0757ba9d2170cf'
